3125b8d82ce29199145818511b5bd9a55f8b3d25
flaviooo <flaviooo@gmail.com>
2022-01-20 12:43:00 +0100

    Mese Fixing label reset

--- public/scripts/jsMese.js
+++ public/scripts/jsMese.js
@@ -1,21 +1,21 @@
 $(document).ready(function () {
-    
+
     _init("tipologiaMeseParam");
-    url = "tipologiaMeseParam" 
+    url = "tipologiaMeseParam"
     $("#stype").on('change', function (event) {
         event.preventDefault();
         console.log(event);
         let queryForm = "tipologiaMeseParam" + this.value
         console.log(queryForm);
         //RGraph.reset($("cvs"));
-        $("#paramID").html("<i>"+queryForm+"</i>")
+        $("#paramID").html("<i>" + queryForm + "</i>")
         callDraw(queryForm)
     });
 });
 
-function _init(url){
+function _init(url) {
     let params = "?meseS=10&meseE=12&anno=2021"
-    $("#paramID").html("<i>"+params+"</i>")
+    $("#paramID").html("<i>" + params + "</i>")
     callDraw(url + params)
 
 }
@@ -61,71 +61,71 @@ function parser(data) {
         dataTemp.push(ob)
     });
     dataJson.push(dataTemp)
-    var labelkey = ["Problema tecnico","Funzionalità aggiuntiva","Richiesta di supporto","Attività di progetto","Licenze/scadenze","Attività generica","Attività documentale"]
-   
+    var labelkey = ["Problema tecnico", "Funzionalità aggiuntiva", "Richiesta di supporto", "Attività di progetto", "Licenze/scadenze", "Attività generica", "Attività documentale"]
+
     var dataMonth = []
     $.each(dataJson, (index, element) => {
-       
+
         var ob = []
         //Elaborao un mese
         for (const key in element) {
             if (element.hasOwnProperty.call(element, key)) {
                 const ele = element[key];
-                var valoriSingolaBar = ["0","0","0","0","0","0"];
+                var valoriSingolaBar = ["0", "0", "0", "0", "0", "0"];
                 console.log(key + " : " + ele)
-                $.each(ele,function(i,e){
-                   
-                  //assegno il valore in percetuale  per singola label
-                  valoriSingolaBar[e[2]-1] = e[4]
-                  console.log("Il valore "+ e[4] +"% è associato al tracker "+ e[3]+ " del mese " +e[0]+
-                   " è stato memorizzato nella posizione "+ [e[2]]+" del singolo Bar "+ valoriSingolaBar[e[2]-1])
-                 //  console.log(" la label del valore con indice "+e[2]+ " = "+valoriSingolaBar[e[2]]+" è associato a "+ labelkey[e[2]])
-                 console.log(" La chiave "+[e[2]]+" identifica il tracker "+labelkey[e[2]-1])
+                $.each(ele, function (i, e) {
+
+                    //assegno il valore in percetuale  per singola label
+                    valoriSingolaBar[e[2] - 1] = e[4]
+                    console.log("Il valore " + e[4] + "% è associato al tracker " + e[3] + " del mese " + e[0] +
+                        " è stato memorizzato nella posizione " + [e[2]] + " del singolo Bar " + valoriSingolaBar[e[2] - 1])
+                    //  console.log(" la label del valore con indice "+e[2]+ " = "+valoriSingolaBar[e[2]]+" è associato a "+ labelkey[e[2]])
+                    console.log(" La chiave " + [e[2]] + " identifica il tracker " + labelkey[e[2] - 1])
                 });
-             }
-            dataMonth.push(valoriSingolaBar) 
+            }
+            dataMonth.push(valoriSingolaBar)
             console.log("nuovo mese ")
         }
     });
     console.log(listaMesiPresenti)
-    
-    for (i=0;i<dataMonth.length; ++i) {
-        // This function is in RGraph.common.core.js
-        //data[i] = RGraph.log(data[i], 10);
-        console.log(dataMonth[i])
-//        dataMonth[i] = RGraph.log(dataMonth[i], 10);
-    }
-    console.log("RGraph.ObjectRegistry.list() "+ RGraph.ObjectRegistry.list());
+
+    //   for (i=0;i<dataMonth.length; ++i) {
+    // This function is in RGraph.common.core.js
+    //data[i] = RGraph.log(data[i], 10);
+    //      console.log(dataMonth[i])
+    //        dataMonth[i] = RGraph.log(dataMonth[i], 10);
+    //   }
+    console.log("RGraph.ObjectRegistry.list() " + RGraph.ObjectRegistry.list());
     RGraph.ObjectRegistry.clear($("cvs"));
     RGraph.ObjectRegistry.remove($("cvs"));
-    RGraph.reset($("cvs"));       
+    RGraph.reset($("cvs"));
     bar = new RGraph.Bar({
-         id: 'cvs',
-         data: dataMonth,
-         options: {
-             textSize: 10,
-             marginTop: 50,
-             marginBottom: 50,
-             marginLeft: 50,
-             xaxisLabelsAngle: 45,
-             marginInner: .5,
-             xaxisLabelsOffsety: 10,
-             xaxisLabels: listaMesiPresenti,
-             linewidth: 2,
-             //colorsStroke: 'white',
+        id: 'cvs',
+        data: dataMonth,
+        options: {
+            textSize: 10,
+            marginTop: 50,
+            marginBottom: 50,
+            marginLeft: 50,
+            xaxisLabelsAngle: 45,
+            marginInner: .5,
+            xaxisLabelsOffsety: 10,
+            xaxisLabels: listaMesiPresenti,
+            linewidth: 2,
+            //colorsStroke: 'white',
             // colors: ['#aaf', 'rgba(96,128,96,0.5)', '#faa','#afa'],
-             backgroundGridVlines: false,
-             backgroundGridBorder: false,
-             grouping: 'stacked',
-             colorsStroke: 'transparent',
-             shadow: false,
-             labelsAbove: true,
-             labelsAboveDecimals: 2,
-             //tooltips: '<table border="1" align="center" cellspacing="0"><tr><td>%%{index}</td><td>%{index}</td></tr><tr><td>%%{dataset}</td><td>%{dataset}</td></tr><tr><td>%%{group}</td><td>%{group}</td></tr><tr><td>%%{sequential_index}</td><td>%{sequential_index}</td></tr><tr><td>%%{seq}</td><td>%{seq}</td></tr><tr><td>%%{value}</td><td>%{value}</td></tr><tr><td>%%{value_formatted}</td><td>%{value_formatted}</td></tr><tr><td>%%{property:xaxisLabels[%%{group}]}</td><td>%{property:xaxisLabels[%{group}]}</td></tr><tr><td>%%{prop:title}</td><td>%{prop:title}</td></tr><tr><td>%%{property:myNames[%%{group}]} (custom property)</td><td>%{property:myNames[%{group}]}</td></tr><tr><td>%%{function:getDay(%%{index}, %%{group}, %%{seq})}</td><td>%{function:getDay(%{index}, %{group}, %{seq})}</td></tr><tr><td>%%{key}</td><td>%{key}</td></tr></table>',
-             tooltips: '<table border="1" align="center" cellspacing="0"><tr><td>%%{index}</td><td>%{index}</td></tr><tr><td>%%{dataset}</td><td>%{dataset}</td></tr><tr><td>%%{group}</td><td>%{group}</td></tr><tr><td>%%{sequential_index}</td><td>%{sequential_index}</td></tr><tr><td>%%{seq}</td><td>%{seq}</td></tr><tr><td>%%{value}</td><td>%{value}</td></tr><tr><td>%%{value_formatted}</td><td>%{value_formatted}</td></tr><tr><td>%%{property:xaxisLabels[%%{group}]}</td><td>%{property:xaxisLabels[%{group}]}</td></tr><tr><td>%%{prop:title}</td><td>%{prop:title}</td></tr><tr><td>%%{property:myNames[%%{group}]} (custom property)</td><td>%{property:myNames[%{group}]}</td></tr><tr><td>%%{function:getDataTracker(%%{index}, %%{group}, %%{seq})}</td><td>%{function:getDataTracker(%{index}, %{group}, %{seq})}</td></tr><tr><td>%%{key}</td><td>%{key}</td></tr></table>',
-        //    tooltipsFormattedKeyLabels:['Alf','Bert','Carl'],
-        tooltipsFormattedKeyLabels:listaMesiPresenti,
-      //tooltipsFormattedKeyColors:['pink','gray','blue'],
+            backgroundGridVlines: false,
+            backgroundGridBorder: false,
+            grouping: 'stacked',
+            colorsStroke: 'transparent',
+            shadow: false,
+            labelsAbove: true,
+            labelsAboveDecimals: 2,
+            //tooltips: '<table border="1" align="center" cellspacing="0"><tr><td>%%{index}</td><td>%{index}</td></tr><tr><td>%%{dataset}</td><td>%{dataset}</td></tr><tr><td>%%{group}</td><td>%{group}</td></tr><tr><td>%%{sequential_index}</td><td>%{sequential_index}</td></tr><tr><td>%%{seq}</td><td>%{seq}</td></tr><tr><td>%%{value}</td><td>%{value}</td></tr><tr><td>%%{value_formatted}</td><td>%{value_formatted}</td></tr><tr><td>%%{property:xaxisLabels[%%{group}]}</td><td>%{property:xaxisLabels[%{group}]}</td></tr><tr><td>%%{prop:title}</td><td>%{prop:title}</td></tr><tr><td>%%{property:myNames[%%{group}]} (custom property)</td><td>%{property:myNames[%{group}]}</td></tr><tr><td>%%{function:getDay(%%{index}, %%{group}, %%{seq})}</td><td>%{function:getDay(%{index}, %{group}, %{seq})}</td></tr><tr><td>%%{key}</td><td>%{key}</td></tr></table>',
+            tooltips: '<table border="1" align="center" cellspacing="0"><tr><td>%%{index}</td><td>%{index}</td></tr><tr><td>%%{dataset}</td><td>%{dataset}</td></tr><tr><td>%%{group}</td><td>%{group}</td></tr><tr><td>%%{sequential_index}</td><td>%{sequential_index}</td></tr><tr><td>%%{seq}</td><td>%{seq}</td></tr><tr><td>%%{value}</td><td>%{value}</td></tr><tr><td>%%{value_formatted}</td><td>%{value_formatted}</td></tr><tr><td>%%{property:xaxisLabels[%%{group}]}</td><td>%{property:xaxisLabels[%{group}]}</td></tr><tr><td>%%{prop:title}</td><td>%{prop:title}</td></tr><tr><td>%%{property:myNames[%%{group}]} (custom property)</td><td>%{property:myNames[%{group}]}</td></tr><tr><td>%%{function:getDataTracker(%%{index}, %%{group}, %%{seq})}</td><td>%{function:getDataTracker(%{index}, %{group}, %{seq})}</td></tr><tr><td>%%{key}</td><td>%{key}</td></tr></table>',
+            //    tooltipsFormattedKeyLabels:['Alf','Bert','Carl'],
+            tooltipsFormattedKeyLabels: listaMesiPresenti,
+            //tooltipsFormattedKeyColors:['pink','gray','blue'],
             tooltipsPointer: false,
             tooltipsPositionStatic: false,
             tooltipsCss: {
@@ -133,49 +133,62 @@ function parser(data) {
                 color: 'black'
             },
             key: labelkey,
-             keyPosition: 'margin',
-             keyPositionX: 50,
-             keyPositionY: 18,
-             keyPositionMarginBoxed: true,
-             xaxis: false,
-             yaxis: false,
-         }
-     }).on('draw', function (obj)
-     {
-         for (var i=0; i<obj.coords.length; ++i) {
-             
-             //Thse are the coordinates of the bar
-             var x      = obj.coords[i][0],
-                 y      = obj.coords[i][1],
-                 width  = obj.coords[i][2],
-                 height = obj.coords[i][3];
- 
-             obj.context.fillStyle = 'white';
-             
-             var ret = RGraph.text({
-                 object: obj,
-                 font:   'Verdana',
-                 size:   10,
-                 x:      x + (width/ 2),
-                 y:      y + (height / 2),
-                 text:   obj.data_arr[i].toString(),
-                 valign: 'center',
-                 halign: 'center'
-             });
-             
-           //  ret.node.style.textShadow = '-2px -2px 1px black, 2px 2px 1px black';
-         }
-     }).draw();
-     /* .responsive([
-        {maxWidth: null,width:600,height:300,options: {xaxisLabelsOffsety:0,xaxisLabelsAngle:0,textSize: 14},css:{'float':'right'}},
-        {maxWidth: 900,width:400,height:200,options: {xaxisLabelsOffsety:5,xaxisLabelsAngle:45,textSize: 10},css:{'float':'none'}}
-    ]); */
-    
-}
-function getDataTracker (index, group, seq)
-    {
-       // var days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-        var labelkey = ["Problema tecnico (bug)","Funzionalità aggiuntiva","Richiesta di supporto","Attività di progetto","Licenze e scadenze","Attività generica"]
-  
-        return labelkey[index] + " (indexes: index=" + index + ", group=" + group + ", seq=" + seq + ")";
+            keyPosition: 'margin',
+            keyPositionX: 50,
+            keyPositionY: 18,
+            keyPositionMarginBoxed: true,
+            xaxis: false,
+            yaxis: false,
+        }
+    }).on('draw', function (obj) {
+        console.log("RGraph.ObjectRegistry.list() " + RGraph.ObjectRegistry.list());
+
+        for (var i = 0; i < obj.coords.length; ++i) {
+
+            //Thse are the coordinates of the bar
+            var x = obj.coords[i][0],
+                y = obj.coords[i][1],
+                width = obj.coords[i][2],
+                height = obj.coords[i][3];
+
+            obj.context.fillStyle = 'white';
+
+            var ret = RGraph.text({
+                object: obj,
+                font: 'Verdana',
+                size: 10,
+                x: x + (width / 2),
+                y: y + (height / 2),
+                text: obj.data_arr[i].toString(),
+                valign: 'center',
+                halign: 'center'
+            });
+
+            //  ret.node.style.textShadow = '-2px -2px 1px black, 2px 2px 1px black';
+        }
+    }).draw()//;
+    /* .responsive([
+       {maxWidth: null,width:600,height:300,options: {xaxisLabelsOffsety:0,xaxisLabelsAngle:0,textSize: 14},css:{'float':'right'}},
+       {maxWidth: 900,width:400,height:200,options: {xaxisLabelsOffsety:5,xaxisLabelsAngle:45,textSize: 10},css:{'float':'none'}}
+   ]); */
+   .exec(function (obj)
+{
+    // Calculate marginInner here,set it and call 
+    RGraph.redraw();
+});
+    delay = 2000;
+    function update() {
+        var value = Math.random(0, 1000);
+
+        draw(value * 10000);
+
+        // Call ourselves again
+        setTimeout(update, delay);
     }
+
+    setTimeout(update, delay);
+}
+function getDataTracker(index, group, seq) {
+    var labelkey = ["Problema tecnico (bug)", "Funzionalità aggiuntiva", "Richiesta di supporto", "Attività di progetto", "Licenze e scadenze", "Attività generica"]
+    return labelkey[index] + " (indexes: index=" + index + ", group=" + group + ", seq=" + seq + ")";
+}
