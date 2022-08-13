9245665a1c0fbcf7386cef5ba6bb4f49c4da15dd
flaviooo <flaviooo@gmail.com>
2022-08-13 23:42:10 +0200

    test

---
+++ test/connectSSH_FTP.js
@@ -0,0 +1,33 @@
+const expect = require('chai').expect
+
+//const config = require('./../config/config');
+//const config = require('dotenv').config({ path: './../.env' });
+const fs = require('fs');
+let Client = require('ssh2-sftp-client');
+let sftp = new Client();
+
+sftp.connect({
+  host: '95.157.76.170',
+  port: '22',
+  username: 'admincsea',
+  password: 'Cse4R3dmine2022!'
+}).then(() => {
+  return sftp.list('./dump');
+}).then(data => {
+  console.log(data, 'the data info');
+}).catch(err => {
+  console.log(err, 'catch error');
+});
+
+
+let config_CDLAN = {
+  connSettings : {
+    port: process.env.CDLAN_PORT || 22, 
+    host:  process.env.CDLAN_HOST || "95.157.76.170",
+    username: process.env.CDLAN_USERNAME || 'admincsea',
+    interactiveAuth: true,
+    tryKeyboard: true,
+    password : process.env.CDLAN_PASS || 'Cse4R3dmine2022!',
+    privateKey:  fs.readFileSync(process.env.CDLAN_PATH_PEM || '../etc/key/id_rsa.pem')
+  }
+};
