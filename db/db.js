4eefe11847ea641e03d611a9cbe3ea2dab7ad895
flaviooo <flaviooo@gmail.com>
2022-09-04 21:11:55 +0200

    cdlan

--- db/db.js
+++ db/db.js
@@ -1,5 +1,5 @@
 //const config_db = require('./config_db');
-const config = require('../config/config');
+const config = require('../config/config.js');
 const mysql = require('mysql');
 
 const conn_db = mysql.createConnection(config.config_db.connDBsettings);
