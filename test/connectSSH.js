becc2550348bb6b2216b69600f271a4273df0ec1
flaviooo <flaviooo@gmail.com>
2022-09-12 00:43:35 +0200

    ò

--- test/connectSSH.js
+++ test/connectSSH.js
@@ -4,7 +4,7 @@ const expect = require('chai').expect
 //const config = require('./../config/config');
 const config = require('dotenv').config({ path: './../.env' });
 const fs = require('fs');
-//console.log("../"+process.env.CDLAN_PATH_PEM)
+console.log("../"+process.env.CDLAN_PATH_PEM)
 
 let config_CDLAN = {
  
