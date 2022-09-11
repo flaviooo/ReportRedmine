254f081ba7338b3668f33915651967bb1ffda352
flaviooo <flaviooo@gmail.com>
2022-09-11 23:08:12 +0200

    ts

--- test/axiosRedmine.js
+++ test/axiosRedmine.js
@@ -2,11 +2,13 @@ require('dotenv').config({ path: './../.env' });
 const axios = require('axios');
 async function getTimeEntriesXML(){
     
+    console.log(process.env.CDLAN_URL_API);
+
     const connAPI = {
         method: 'get',
         url: process.env.CDLAN_URL_API || 'http://192.168.40.30:3000/time_entries.xml',
         headers: { 'X-Redmine-API-Key': process.env.CDLAN_PASS_API || 'password' },
-        password : process.env.CDLAN_PASS_API || 'admincsea'
+    //    password : process.env.CDLAN_PASS_API || 'admincsea'
     }  
     let res = await axios(connAPI)
  //   console.log(res.request._header);
