af0bac720664dee95ba4ee51b7ea881e160b0dc6
fL <flaviooo@gmail.com>
2025-11-05 01:30:45 +0100

    settaggio per il dump

--- importDB.sh
+++ importDB.sh
@@ -2,7 +2,23 @@
 
 password=$passwd
 # pathHome=/tmp/home/admincsea/dump/archivio
-sqlfile=backup_$(date +%d-%m-%Y).sql
+
+# Controlla che sia stato passato un parametro
+if [ -z "$1" ]; then
+  echo "Uso: $0 nome_file.sql"
+  exit 1
+fi
+
+# Nome file passato da parametro
+sqlfile="$1"
+
+# Verifica che il file esista
+if [ ! -f "$sqlfile" ]; then
+  echo "Errore: il file '$sqlfile' non esiste."
+  exit 1
+fi
+
+#sqlfile=backup_$(date +%d-%m-%Y).sql
 mysql --default-character-set='utf8' bitnami_redmine < $sqlfile
 echo $(date -u) " Import effettuato. "
 #cd $pathHome/archivio
